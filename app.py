dfbb744f0a38466bc89ceca88865598efbd730ad
Markkzxc <tenio.markdave@cec.edu.ph>
2025-05-06 17:42:23 +0800

    first commitsss

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on Heroku!"
+    return "Mark Dave Tenio \n BSIT-III-2nd 25 \n System Integration \n Semi-final Exam"
 
 if __name__ == "__main__":
     port = int (os.environ.get("PORT",5000))
